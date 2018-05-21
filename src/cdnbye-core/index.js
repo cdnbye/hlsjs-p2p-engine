396d3005289ec92ed8283d7d86783f769e47269a
谢庭 <86755838@qq.com>
2018-05-21 19:43:33 +0800

    first commit

---
+++ src/cdnbye-core/index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by xieting on 2018/4/3.
+ */
+
+// import datachannel from './datachannel';
+// import events from './events';
+// import fetcher from './fetcher';
+// import  getBrowserRTC from 'get-browser-rtc';
+//
+// var Buffer = require('buffer/').Buffer;
+//
+// export {
+//     datachannel as DataChannel,
+//     events as Events,
+//     fetcher as Fetcher,
+//     Buffer,
+//     getBrowserRTC
+// }
+
+
+export * from 'cdnbye-core';
