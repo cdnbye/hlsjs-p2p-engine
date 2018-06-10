5c85dc0e34acfc49a11c06e4d0e3284bae7cea44
snowinszu <86755838@qq.com>
2018-06-10 19:13:03 +0800

    fix bug

--- src/cdnbye-core/index.js
+++ src/cdnbye-core/index.js
@@ -2,20 +2,20 @@
  * Created by xieting on 2018/4/3.
  */
 
-import datachannel from './cdnbye-core/src/datachannel';
-import events from './cdnbye-core/src/events';
-import fetcher from './cdnbye-core/src/fetcher';
-import  getBrowserRTC from 'get-browser-rtc';
+// import datachannel from './cdnbye-core/src/datachannel';
+// import events from './cdnbye-core/src/events';
+// import fetcher from './cdnbye-core/src/fetcher';
+// import  getBrowserRTC from 'get-browser-rtc';
+//
+// var Buffer = require('buffer/').Buffer;
+//
+// export {
+//     datachannel as DataChannel,
+//     events as Events,
+//     fetcher as Fetcher,
+//     Buffer,
+//     getBrowserRTC
+// }
 
-var Buffer = require('buffer/').Buffer;
 
-export {
-    datachannel as DataChannel,
-    events as Events,
-    fetcher as Fetcher,
-    Buffer,
-    getBrowserRTC
-}
-
-
-// export * from 'cdnbye-core';
+export * from 'cdnbye-core';
